4f3cc345479f16e4690731dc262541e91691d21f
luis-mcr <luismcruzadoruiz@gmail.com>
2022-11-24 13:00:13 -0300

    Header terminado

---
+++ ProyectoAP_LuisCruzado/src/app/component/rrss/rrss.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RRSSComponent } from './rrss.component';
+
+describe('RRSSComponent', () => {
+  let component: RRSSComponent;
+  let fixture: ComponentFixture<RRSSComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RRSSComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RRSSComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ProyectoAP_LuisCruzado/src/app/component/rrss/rrss.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rrss',
+  templateUrl: './rrss.component.html',
+  styleUrls: ['./rrss.component.css']
+})
+export class RRSSComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
