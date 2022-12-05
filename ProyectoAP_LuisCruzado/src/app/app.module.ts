c865ae893f491aa37772c0e89d5dba1ab8562db6
luis-mcr <luismcruzadoruiz@gmail.com>
2022-12-05 20:33:43 -0300

    Frontend Final

--- ProyectoAP_LuisCruzado/src/app/app.module.ts
+++ ProyectoAP_LuisCruzado/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { LogoAPComponent } from './component/logo-ap/logo-ap.component';
-import { RRSSComponent } from './component/rrss/rrss.component';
 import { BannerComponent } from './component/banner/banner.component';
 import { PresentacionComponent } from './component/presentacion/presentacion.component';
 import { ExperienciaComponent } from './component/experiencia/experiencia.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './component/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    RRSSComponent,
     BannerComponent,
     PresentacionComponent,
     ExperienciaComponent,
